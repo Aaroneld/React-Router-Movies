6b7a48bbd46286adf4f825095661903d2699d443
Aaron Merrifield-Lucier <merrifield48@gmail.com>
2019-10-06 20:48:15 -0400

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(false);
+  
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,6 +14,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          
         })
         .catch(error => {
           console.error(error);
@@ -27,23 +29,26 @@ const Movie = (props) => {
   // }
 
   if (!movie) {
+    
     return <div>Loading movie information...</div>;
+
   }
 
-  const { title, director, metascore, stars } = movie;
+  
   return (
+    
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
+        <h2>{movie["title"]}</h2>
         <div className="movie-director">
-          Director: <em>{director}</em>
+          Director: <em>{movie["director"]}</em>
         </div>
         <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
+          Metascore: <strong>{movie["metascore"]}</strong>
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {movie["stars"].map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
